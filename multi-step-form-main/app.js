29982f25af90a6bf64c68635bcd65840197b5600
codesnow <engakinkunmi@gmail.com>
2023-03-03 04:57:04 +0100

    Consistency

--- multi-step-form-main/app.js
+++ multi-step-form-main/app.js
@@ -4,116 +4,131 @@
     const itemBox = sidebar.querySelectorAll(".item-id")
     const sectionBox = contentBox.querySelectorAll(".content");
      const btns = footer.querySelectorAll(".btn")
-
+    const form = document.querySelector(".form");
     let count = 0;
     let  itemArrayBox= [];
     let contentArrayBox = [];
 
     // Array generator function
- function arrayFilter (arrayTest,arrayBox,condition) {
-    arrayTest.forEach((item) => {
-        const category = item.dataset.id.includes(condition)
-        if (category) {
-         arrayBox.push(item.dataset.id)
-        } 
-    }) 
+//  function arrayFilter (arrayTest,arrayBox,condition) {
+//     arrayTest.forEach((item) => {
+//         const category = item.dataset.id.includes(condition)
+//         if (category) {
+//          arrayBox.push(item.dataset.id)
+//         } 
+//     }) 
    
- }
+//  }
 
-   
+//  arrayFilter(itemBox,itemArrayBox,"step-")
+//  arrayFilter(sectionBox,contentArrayBox,"step-")
+
+
+//     btns.forEach((btn) => {
+//         btn.addEventListener("click", (e) => {
+//             const key = e.currentTarget.classList;
+//             if (key.contains("next-btn")) {
+
+//                 count++               
+//                 if (contentArrayBox[count] !== itemArrayBox[count]) {
+                           
+//                     for (const item of sectionBox) {
+//                        if (item.dataset.id === "step-5") {
+//                            if (!item.classList.contains("active")) {
+//                                item.classList.add("active")
+//                            } else {
+//                                item.classList.remove("active")
+//                            }
+//                        }
+//                     }
+//                 } 
+//                 else  {
+                           
+//                     if (contentArrayBox[count] === itemArrayBox[count]) {
+                     
+//                         console.log(count)
+                   
+                      
+                 
+//         }
+//        } 
+         
+//             }
+//         else if (key.contains("prev-btn")) {
+//                 if (count === 0 || count === contentArrayBox.length && count > itemArrayBox.length) {
+//                     count = contentArrayBox.length
+//                 } 
+//                    count--
+//                 //  console.log(count)
+//                 if (count < itemArrayBox.length) {
+               
+//                     activeFunction(itemBox)
+//                     activeFunction(sectionBox)
+//             }
+//         }
+             
+//         })
+       
+//     })
 
 
 
 
-    window.addEventListener("DOMContentLoaded", () => {
-        btns.forEach((btn) => {
-            btn.addEventListener("click", (e) => {
-                const key = e.currentTarget.classList;
-                arrayFilter(itemBox,itemArrayBox,"step-")
-                arrayFilter(sectionBox,contentArrayBox,"step-")
+//         function activeFunction(activeBox) {
+//             for (const item of activeBox) {
                 
-                if (key.contains("next-btn")) {
-                //    console.log(itemBox)
-                //    console.log(sectionBox)
-                    count++               
-                    if (contentArrayBox[count] === itemArrayBox[count]) {
-                            console.log(contentArrayBox[count])
-                            console.log(itemArrayBox[count])
-                            // console.log(count)
-                          
-                       
-                                
-
-                        } else if (contentArrayBox[count] !== itemArrayBox[count]) {
-                        //  console.log(count)
-                        console.log(contentArrayBox[count]);
-                            // console.log(count)
-                        
-                     }else {
+//                     if (count === 3) {  
+//                     if ( item.dataset.id === "step-3") {   
+//                         item.classList.remove('active')
+//                     }
+//                     if ( item.dataset.id === "step-4") {   
+//                         item.classList.add("active") 
+//                     }
+//                     }
+//                     else if (count === 2) {
+//                         if ( item.dataset.id === "step-2") {
+//                             item.classList.remove("active")
+//                         }
+//                         if ( item.dataset.id === "step-3") {
+//                             item.classList.add("active")
+//                         }
                     
-                     }
+//                         }
+//                     else  if (count === 1) {
+//                             if ( item.dataset.id === "step-1") { 
+//                                 item.classList.remove("active")
+                            
+//                             }
+//                         if ( item.dataset.id === "step-2") {
+//                                 item.classList.add("active")
+//                             }
+//                             }
+                                                           
+            
+//             }
+//         }
+
+                      
+                        
                 
+                           
           
 
-                } else if (key.contains("prev-btn")) {
-                    // count--
-                
-                }
-                // console.log(count)
-            })
-        })
-    })
+      
+                             
+                        
+                     
+            for (const btn of btns) {
+                btn.addEventListener("click", (e) => {
+                    if (e.currentTarget.classList.contains("prev-btn")) {
+                      count--
+                   
+                    } else {
+                       count++
+                       console.log(sectionBox)
+                    }
+                })
+            }
 
 
- 
-
-    // for (let i = 0; i < itemBox.length; i++) {
-    //  if (itemBox[i].classList.contains("active")) {
-    //     //   itemBox[i].classList.remove("active")
-    //      console.log(i)
-    //  } else {
-    //     // itemBox[i].classList.add("active")
-    //     console.log(i)
-    //  }
-       
-    // }
-
-
-// console.log(itemBox)
-    // itemBox.forEach((item) => {
-    //     const active = item.classList.contains("active")
-    //     if (active) {
-    //         item.classList.remove("active")
-    //     } else {
-    //         // if ()
-    //     }
-    // })
-
-        
-    // for (let count = 0; count < contentArrayBox.length; count++) {
-     
-       
-    //  }
-
-
-
-
-
-
-
-
-
-
-
-    // for (let i = 0; i < contentArrayBox.length; i++) {
-     
-    //    if (contentArrayBox[i] === itemArrayBox[i]) {
-    //         console.log(i)
-    //    } else if (contentArrayBox[i] !== itemArrayBox[i]) {
-    //     console.log(i)
-    // }  
-    // }
-
-
-
-
+          
